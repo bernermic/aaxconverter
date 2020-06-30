7209b2372d13141eb994fdd27ceda2d629cee3fa
Michael Berner <michael.berner@breuninger.de>
2020-06-30 13:54:09 +0200

    More flexibility and adapt readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aaxconverter
+## Prerequisites
+* needs ffmpeg installed
+* needs your audible activationcode to work
+
+## Install
+Just download convert.sh into a binary folder of your choice - for example $HOME/.local/bin
--- convert.sh
+++ convert.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 sourcefile=$1
 targetfile=$2
 activation=xxx
-path=`pwd`
+[ -n "$3" ] && activation=$3
+path=$(pwd) 
 
-[ $activation = "xxx" ] && ( echo "please insert your personal activation code"; exit 1 )
+[ "$activation" = "xxx" ] && ( echo "please set your personal activation code as activation=<yourcode>"; exit 1 )
 
 help()
 {
-    echo "Usage: `basename $0` [sourcefile targetfile]"
+    echo "Usage: `basename $0` sourcefile targetfile [activationcode]"
     exit 0
 }
 
@@ -22,10 +23,13 @@ metadata()
 convert()
 {
     cd "$path"
-    [ -s "$targetfile.meta" ] && ffmpeg -activation_bytes "$activation" -i "$sourcefile" -i "$targetfile.meta" -map_metadata 1 -codec copy "$targetfile" || echo "No metadata found"
+    [ -s "$targetfile.meta" ] && ffmpeg -activation_bytes "$activation" -i "$sourcefile" -i "$targetfile.meta" -map_metadata 1 -codec copy "$targetfile.m4a" || echo "No metadata found"
+    rm "$targetfile.meta"
 }
 
-[ -z $sourcefile ] || [ -z $targetfile ] && help
+[ -z "$sourcefile" ] || [ -z "$targetfile" ] && help
+[ ! -f "$sourcefile" ] && ( echo "File $sourcefile not found. Please check you gave the correct parameter"; exit 1 )
+[ -d "$(dirname $sourcefile)" ] && path=$(dirname $sourcefile) 
 metadata
 convert
 
